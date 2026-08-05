628cc1f70e83ad0860f067ea792ba50d32171791
koreaone10-del <koreaone10@gmail.com>
2026-08-05 17:53:29 +0100

    Create products.js with product details
    Added a products database with sample products.

---
+++ products.js
@@ -0,0 +1,17 @@
+// ملف products.js
+const productsDatabase = {
+    "1": {
+        babaId: "NL-94", // كود بابا الجزائر
+        name: "سماعات الرأس اللاسلكية الاحترافية",
+        price: 1500, // سعرك
+        image: "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?w=500&q=80"
+    },
+    "2": {
+        babaId: "XY-55",
+        name: "عصا السيلفي الذكية مع إضاءة",
+        price: 2200,
+        image: "https://images.unsplash.com/photo-1527011045970-128a30a21fb9?w=500&q=80"
+    }
+    // يمكنك إضافة "3" و "4" بنفس الطريقة بسهولة
+};
+
